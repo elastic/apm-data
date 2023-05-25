601bf6e402f9239836f73dda0a637e864fb19265
kruskal <99559985+kruskall@users.noreply.github.com>
2023-05-25 07:00:25 +0200

    build: add scripts for protobuf installation and model generation

---
+++ tools/install-protobuf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+PLATFORM=$(go env GOOS)
+PROTOBUF_VERSION="23.1"
+PROTOC_GO_VERSION="v1.30.0"
+
+
+if [ "${PLATFORM}" = "darwin" ]; then
+	PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-osx-x86_64.zip"
+elif [ "${PLATFORM}" = "linux" ]; then 
+	PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-x86_64.zip"
+elif [ "${PLATFORM}" = "windows" ]; then 
+	PROTOBUF_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-win64.zip"
+else
+	echo "Unsupported platform: ${PLATFORM}"
+	exit 1
+fi
+
+TOOLS_DIR=$(dirname "$(readlink -f -- "$0")")
+PROTOBUF_ZIP="/tmp/protobuf.zip"
+
+curl -L "${PROTOBUF_URL}" -o "${PROTOBUF_ZIP}"
+
+if ! unzip -j -o "${PROTOBUF_ZIP}" "bin/protoc" -d "${TOOLS_DIR}/bin"; then
+	echo "failed to extract protobuf"
+	exit 1
+fi
+
+if ! PATH="${TOOLS_DIR}/bin" protoc --version; then 
+	echo "failed to verify protobuf installation"
+	exit 1
+fi
+
+GOBIN="${TOOLS_DIR}/bin" go install "google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GO_VERSION}"
