eb14f16d008364cd71a23e85ef4e5d93ad9104a3
Andrew Wilkins <axw@elastic.co>
2023-06-20 12:32:48 +0800

    Fix protoc installation (#77)
    Unpack the full protoc zip to ensure the includes can be found.
    Ensure tools/build/bin is first in $PATH, so the protoc and friends
    are found there rather than somewhere like /usr/bin.

--- tools/generate-modelpb.sh
+++ tools/generate-modelpb.sh
@@ -2,4 +2,4 @@
 
 TOOLS_DIR=$(dirname "$(readlink -f -- "$0")")
 
-PATH="${PATH}:${TOOLS_DIR}/bin" protoc --proto_path=./model/proto/ --go_out=. --go_opt=module=github.com/elastic/apm-data --go-vtproto_out=. --go-vtproto_opt=features=marshal+unmarshal+size+pool,module=github.com/elastic/apm-data ./model/proto/*.proto
+PATH="${TOOLS_DIR}/build/bin:${PATH}" protoc --proto_path=./model/proto/ --go_out=. --go_opt=module=github.com/elastic/apm-data --go-vtproto_out=. --go-vtproto_opt=features=marshal+unmarshal+size+pool,module=github.com/elastic/apm-data ./model/proto/*.proto
