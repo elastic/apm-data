fb217c7f06d6b10bbe9a66208091b01c2200aa81
kruskal <99559985+kruskall@users.noreply.github.com>
2023-09-20 16:35:43 +0200

    docs: clarify batch processor doc

--- model/modelpb/batch.go
+++ model/modelpb/batch.go
@@ -27,10 +27,10 @@ type BatchProcessor interface {
 	// Processing may involve anything, e.g. modifying, adding, removing,
 	// aggregating, or publishing events.
 	//
-	// The caller should not assume the batch to be valid after the
-	// method has returned.
+	// The caller should not assume the batch or any events inside of it
+	// to be valid after the method has returned.
 	// If the batch needs to be processed asynchronously or kept around,
-	// the processor must create a copy of the slice.
+	// the processor must clone the batch using the Clone method.
 	ProcessBatch(context.Context, *Batch) error
 }
 
