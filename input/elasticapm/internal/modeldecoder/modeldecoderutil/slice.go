8d1e09360588f5a272c0e5e86eeebe3fb3c4989f
kruskal <99559985+kruskall@users.noreply.github.com>
2023-09-28 17:03:37 +0200

    docs: improve reslice documentation

--- input/elasticapm/internal/modeldecoder/modeldecoderutil/slice.go
+++ input/elasticapm/internal/modeldecoder/modeldecoderutil/slice.go
@@ -17,10 +17,11 @@
 
 package modeldecoderutil
 
-// Reslice increases the slice's capacity, if necessary, to match n.
-// If specified, the newFn function is used to create the elements
-// to populate the additional space appended to the slice.
+// Reslice increases the slice's capacity, if necessary, to guarantee space for n elements.
+// If specified, the newFn function is used to populate the extra (optionally) allocated space.
+// The method returns a slice with len(slice)==n.
 func Reslice[Slice ~[]model, model any](slice Slice, n int, newFn func() model) Slice {
+	// check if there is enough capacity to hold n elements
 	if diff := n - cap(slice); diff > 0 {
 		// start of the extra space
 		idx := cap(slice)
@@ -31,6 +32,7 @@ func Reslice[Slice ~[]model, model any](slice Slice, n int, newFn func() model) 
 		slice = append([]model(slice)[:cap(slice)], make([]model, diff)...)
 		if newFn != nil {
 			// extend the slice to its capacity
+			// Note: reset the slice length to n before returning
 			slice = slice[:cap(slice)]
 
 			// populate the extra space
@@ -40,5 +42,9 @@ func Reslice[Slice ~[]model, model any](slice Slice, n int, newFn func() model) 
 		}
 	}
 
+	// slice has enough capacity to hold n elements and there is no need to grow the slice.
+	// Do not make assumption on the length of the slice and always return a slice with len(slice)==n.
+	// Most of the time len(slice)==0 because the argument is a newly reset slice from a
+	// protobuf model.
 	return slice[:n]
 }
