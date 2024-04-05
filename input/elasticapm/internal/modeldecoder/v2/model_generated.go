ae4ebd3c0cba6c938dfe26b4b3f834ca633d9853
Jonas Kunz <jonas.kunz@elastic.co>
2024-04-05 12:28:09 +0200

    Regenerate files

--- input/elasticapm/internal/modeldecoder/v2/model_generated.go
+++ input/elasticapm/internal/modeldecoder/v2/model_generated.go
@@ -554,7 +554,7 @@ func (val *metadataCloudService) processNestedSource() error {
 }
 
 func (val *metadataSystem) IsSet() bool {
-	return val.Architecture.IsSet() || val.ConfiguredHostname.IsSet() || val.Container.IsSet() || val.DetectedHostname.IsSet() || val.DeprecatedHostname.IsSet() || val.Kubernetes.IsSet() || val.Platform.IsSet() || val.HostId.IsSet()
+	return val.Architecture.IsSet() || val.ConfiguredHostname.IsSet() || val.Container.IsSet() || val.DetectedHostname.IsSet() || val.DeprecatedHostname.IsSet() || val.Kubernetes.IsSet() || val.Platform.IsSet() || val.HostID.IsSet()
 }
 
 func (val *metadataSystem) Reset() {
@@ -565,7 +565,7 @@ func (val *metadataSystem) Reset() {
 	val.DeprecatedHostname.Reset()
 	val.Kubernetes.Reset()
 	val.Platform.Reset()
-	val.HostId.Reset()
+	val.HostID.Reset()
 }
 
 func (val *metadataSystem) validate() error {
@@ -594,7 +594,7 @@ func (val *metadataSystem) validate() error {
 	if val.Platform.IsSet() && utf8.RuneCountInString(val.Platform.Val) > 1024 {
 		return fmt.Errorf("'platform': validation rule 'maxLength(1024)' violated")
 	}
-	if val.HostId.IsSet() && utf8.RuneCountInString(val.HostId.Val) > 1024 {
+	if val.HostID.IsSet() && utf8.RuneCountInString(val.HostID.Val) > 1024 {
 		return fmt.Errorf("'host_id': validation rule 'maxLength(1024)' violated")
 	}
 	return nil
