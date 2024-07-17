933c168818ff3452badcbfb7060ceaed4de3d8b6
Carson Ip <carsonip@users.noreply.github.com>
2024-07-17 15:23:20 +0100

    Fix host.ip 0.0.0.0 treated as invalid (#323)
    `host.ip` is parsed from resource attribute `ip` when resource attribute `opencensus.exporterversion` has prefix "Jaeger". This PR fixes the incorrect handling of value `0.0.0.0` of this field.

--- model/modelpb/ip.go
+++ model/modelpb/ip.go
@@ -41,6 +41,7 @@ func MustParseIP(s string) *IP {
 	return ip
 }
 
+// Addr2IP converts a valid netip.Addr to IP.
 func Addr2IP(addr netip.Addr) *IP {
 	if addr.Is4() {
 		ip := IPFromVTPool()
@@ -53,16 +54,17 @@ func Addr2IP(addr netip.Addr) *IP {
 	return ip
 }
 
+// IP2Addr converts a nil IP to a zero netip.Addr and a valid IP to a valid netip.Addr.
 func IP2Addr(i *IP) netip.Addr {
+	if i == nil {
+		return netip.Addr{}
+	}
 	if addr := i.GetV6(); len(addr) == 16 {
 		return netip.AddrFrom16([16]byte(addr))
 	}
-	if i.GetV4() != 0 {
-		var addr [4]byte
-		binary.BigEndian.PutUint32(addr[:], i.V4)
-		return netip.AddrFrom4(addr)
-	}
-	return netip.Addr{}
+	var addr [4]byte
+	binary.BigEndian.PutUint32(addr[:], i.V4)
+	return netip.AddrFrom4(addr)
 }
 
 func IP2String(i *IP) string {
