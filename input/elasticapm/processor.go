e6daa7aef32ebbcb783d9a25f31fd256c3c1b2ec
kruskall <99559985+kruskall@users.noreply.github.com>
2025-05-30 14:31:29 +0200

    feat: add readBatch span (#492)
    * feat: add readBatch span
    
    we've observed large 'Stream' traces with a small semaphore or processor
    usage.
    To improve clarity also trace readBatch since it was observed causing slow
    requests in some instances
    
    * Update processor.go

--- input/elasticapm/processor.go
+++ input/elasticapm/processor.go
@@ -179,6 +179,8 @@ func (p *Processor) readBatch(
 	reader *streamReader,
 	result *Result,
 ) (int, error) {
+	_, sp := p.tracer.Start(ctx, "readBatch")
+	defer sp.End()
 
 	// input events are decoded and appended to the batch
 	origLen := len(*batch)
